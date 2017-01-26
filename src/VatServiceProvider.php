07c84854b69c3fbcc097cc79ac5c9accc9150dfe
Danny van Kooten <dannyvankooten@gmail.com>
2017-01-26 10:02:48 +0100

    catch VIES exception in  validation rule as that service is down an awful lot lately

--- src/VatServiceProvider.php
+++ src/VatServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Container\Container;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Validator as RequestValidator;
 
+use DvK\Laravel\Vat\Vies\ViesException;
 use DvK\Laravel\Vat\Facades\Validator as ValidatorFacade;
 
 
@@ -18,8 +19,22 @@ class VatServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+
+        /**
+         * Register the "vat_number" validation rule.
+         *
+         * When the VIES VAT validation fails, this will just look at the number format.
+         * This can result in invalid VAT numbers being marked as true.
+         * With the VIES VAT number API being down an awful lot lately, we feel it is more important to get the purchase through.
+         */
         RequestValidator::extend('vat_number', function($attribute, $value, $parameters, $validator ) {
-            return ValidatorFacade::validate( $value );
+            try {
+                $valid = ValidatorFacade::validate( $value );
+            }  catch( ViesException $e ) {
+                return true;
+            }
+
+            return $valid;
         });
     }
 
